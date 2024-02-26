d1d17310392a0b8cb31aada987ffe5dacec155e8
ryu-jaehyun <vo6182@gmail.com>
2024-02-26 17:41:02 +0900

    final

--- src/main/java/Project/AiPlanner/Setting/controller/successController.java
+++ src/main/java/Project/AiPlanner/Setting/controller/successController.java
@@ -21,13 +21,14 @@ public class successController {
 
     private final DayPlanService dayPlanService;
     private final MonthPlanService monthPlanService;
-    private  final UserRepository userRepository;
-    @PatchMapping("/success/day")
+    private final UserRepository userRepository;
+
+    @PostMapping("/success")
     @CrossOrigin(origins = "http://ec2-13-125-51-122.ap-northeast-2.compute.amazonaws.com:3000/")
     public ResponseEntity<UpdateSuccessResponseDto> updateSuccess(@RequestBody DaySuccessDto daySuccessDto) {
         String userId = SecurityUtil.getCurrentUserId();
         String userName = userRepository.findUserNameByUserId(userId);
-        log.info("사용자아이디 ={}",userId);
+        log.info("사용자아이디 ={}", userId);
 
         String result = dayPlanService.updateSuccessAndGetAverage(daySuccessDto);
         String message;
@@ -49,33 +50,7 @@ public class successController {
         return ResponseEntity.ok(response);
     }
 
-
-
-
-    @PatchMapping("/success/month")
-    @CrossOrigin(origins = "http://ec2-13-125-51-122.ap-northeast-2.compute.amazonaws.com:3000/")
-    public ResponseEntity<UpdateSuccessResponseDto> updateSuccess(@RequestBody MonthSuccessDto monthSuccessDto) {
-        String userId = SecurityUtil.getCurrentUserId();
-        String userName = userRepository.findUserNameByUserId(userId);
-        log.info("사용자아이디 ={}",userId);
-
-        String result = monthPlanService.updateSuccessAndGetAverage(monthSuccessDto);
-        String message;
-
-        // 결과에 따라 다른 메시지 생성
-        double resultValue = Double.parseDouble(result.replaceAll("[^0-9.]", ""));
-        if (resultValue < 50) {
-            message = "달성률 저조, 분발하세요!";
-        } else if (resultValue < 80) {
-            message = "달성률 좋음. 조금만 더 화이팅!";
-        } else {
-            message = "달성률최고! 꾸준히 유지하세요!";
-        }
-
-        UpdateSuccessResponseDto response = new UpdateSuccessResponseDto();
-        response.setMessage(message);
-        response.setResult(result);
-
-        return ResponseEntity.ok(response);
-    }
 }
+
+
+
